ctaggart/pencil2d

commit aa5c844d99ca7ed9735f609e4974ed53bbe2b409

Reorganize the process of showing Preset Dialog - Using QDialog::open() instead of QDialog::exec() so the Mainwindow is showing at the app start

app/src/mainwindow2.cpp
@@ -89,7 +89,7 @@ GNU General Public License for more details.
 
 
 
-MainWindow2::MainWindow2(QWidget *parent) :
+MainWindow2::MainWindow2(QWidget* parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow2)
 {
@@ -129,6 +129,8 @@ MainWindow2::MainWindow2(QWidget *parent) :
     mEditor->updateObject();
 
     setWindowTitle(PENCIL_WINDOW_TITLE);
+
+    showPresetDialog();
 }
 
 MainWindow2::~MainWindow2()
@@ -488,7 +490,7 @@ void MainWindow2::tabletEvent(QTabletEvent* event)
 
 void MainWindow2::newDocument(bool force)
 {
-    if (force || maybeSave())
+    if (force)
     {
         newObject();
         mEditor->scrubTo(0);
@@ -501,6 +503,28 @@ void MainWindow2::newDocument(bool force)
 
         setWindowTitle(PENCIL_WINDOW_TITLE);
         updateSaveState();
+    }
+    else if (maybeSave())
+    {
+        if (mEditor->preference()->isOn(SETTING::ASK_FOR_PRESET))
+        {
+            showPresetDialog();
+        }
+        else
+        {
+            int defaultPreset = mEditor->preference()->getInt(SETTING::DEFAULT_PRESET);
+            newObjectFromPresets(defaultPreset);
+            mEditor->scrubTo(0);
+            mEditor->view()->resetView();
+            mEditor->select()->resetSelectionProperties();
+
+            // Refresh the palette
+            mColorPalette->refreshColorList();
+            mEditor->color()->setColorNumber(0);
+
+            setWindowTitle(PENCIL_WINDOW_TITLE);
+            updateSaveState();
+        }
     }
 }
 
@@ -710,11 +734,9 @@ bool MainWindow2::saveObject(QString strSavedFileName)
         errorDialog.exec();
         return false;
     }
-    else
-    {
-        mEditor->object()->setFilePath(strSavedFileName);
-        mEditor->object()->setModified(false);
-    }
+
+    mEditor->object()->setFilePath(strSavedFileName);
+    mEditor->object()->setModified(false);
 
     QSettings settings(PENCIL2D, PENCIL2D);
     settings.setValue(LAST_PCLX_PATH, strSavedFileName);
@@ -1019,45 +1041,49 @@ void MainWindow2::resetAndDockAllSubWidgets()
 
 bool MainWindow2::newObject()
 {
-    QString presetFilePath;
-    if (mEditor->preference()->isOn(SETTING::ASK_FOR_PRESET))
-    {
-        PresetDialog presetDialog(mEditor->preference(), this);
-        if (presetDialog.exec() == QDialog::Accepted)
-        {
-            if (presetDialog.shouldAlwaysUse())
-            {
-                mEditor->preference()->set(SETTING::ASK_FOR_PRESET, false);
-                mEditor->preference()->set(SETTING::DEFAULT_PRESET, presetDialog.getPresetIndex());
-            }
-            presetFilePath = presetDialog.getPreset();
-        }
-    }
-    else
-    {
-        int preset = mEditor->preference()->getInt(SETTING::DEFAULT_PRESET);
-        if (preset > 0)
-        {
-            presetFilePath = PresetDialog::getPresetPath(preset);
-        }
-    }
-
     Object* object = nullptr;
-    FileManager fm(this);
+    object = new Object();
+    object->init();
+    mEditor->setObject(object);
+    return true;
+}
+
+bool MainWindow2::newObjectFromPresets(int presetIndex)
+{
+    Object* object = nullptr;   
+    QString presetFilePath = (presetIndex > 0) ? PresetDialog::getPresetPath(presetIndex) : "";
     if (!presetFilePath.isEmpty())
     {
+        FileManager fm(this);
         object = fm.load(presetFilePath);
-        if (!fm.error().ok()) object = nullptr;
+        if (fm.error().ok() == false) object = nullptr;
     }
-
     if (object == nullptr)
     {
         object = new Object();
         object->init();
     }
-    object->setFilePath(QString());
     mEditor->setObject(object);
     return true;
+}
+
+void  MainWindow2::showPresetDialog()
+{
+    if (mEditor->preference()->isOn(SETTING::ASK_FOR_PRESET))
+    {
+        PresetDialog* presetDialog = new PresetDialog(mEditor->preference(), this);
+        presetDialog->setAttribute(Qt::WA_DeleteOnClose);
+        connect(presetDialog, &PresetDialog::finished, [=](int result)
+        {
+            if (result == QDialog::Accepted)
+            {
+                int presetIndex = presetDialog->getPresetIndex();
+                newObjectFromPresets(presetIndex);
+                qDebug() << "Accepted!";
+            }
+        });
+        presetDialog->open();
+    }
 }
 
 void MainWindow2::readSettings()

app/src/mainwindow2.h
@@ -61,7 +61,7 @@ public:
 
     Editor* mEditor = nullptr;
 
-    public slots:
+public slots:
     void undoActSetText();
     void undoActSetEnabled();
     void updateSaveState();
@@ -71,7 +71,6 @@ public:
     void currentLayerChanged();
 
 public:
-    void setOpacity(int opacity);
     void newDocument(bool force = false);
     void openDocument();
     bool saveDocument();
@@ -83,15 +82,15 @@ public:
     void importImage();
     void importImageSequence();
     void importPredefinedImageSet();
-    void importImageSequenceNumbered();
     void importLayers();
     void importMovie();
     void importGIF();
 
     void lockWidgets(bool shouldLock);
 
+    void setOpacity(int opacity);
     void preferences();
-    
+ 
     void openFile(QString filename);
 
     PreferencesDialog* getPrefDialog() { return mPrefDialog; }
@@ -111,6 +110,7 @@ private slots:
 
 private:
     bool newObject();
+    bool newObjectFromPresets(int presetIndex);
     bool openObject(QString strFilename);
     bool saveObject(QString strFileName);
 
@@ -120,6 +120,7 @@ private:
     void setupKeyboardShortcuts();
     void clearKeyboardShortcuts();
     void updateZoomLabel();
+    void showPresetDialog();
 
     void importPalette();
     void exportPalette();
@@ -160,7 +161,7 @@ private:
 
 private:
     ActionCommands* mCommands = nullptr;
-    QList< BaseDockWidget* > mDockWidgets;
+    QList<BaseDockWidget*> mDockWidgets;
 
     QIcon mStartIcon;
     QIcon mStopIcon;

app/src/presetdialog.cpp
@@ -54,15 +54,6 @@ QString PresetDialog::getPresetPath(int index)
     return QString();
 }
 
-int PresetDialog::exec()
-{
-    if (ui->presetComboBox->count() == 1)
-    {
-        return QDialog::Accepted;
-    }
-    return QDialog::exec();
-}
-
 void PresetDialog::initPresets()
 {
     // Make sure the presets directory in the data directory exists and navigate to it
@@ -71,7 +62,8 @@ void PresetDialog::initPresets()
     dataDir.mkdir("presets");
     if (dataDir.cd("presets") == false)
     {
-        accept(); // the presets folder doesn't exist and cannot be created
+        reject(); // the presets folder doesn't exist and cannot be created
+        return;
     }
 
     // Find all presets in the preferences and add them to the combo box
@@ -79,7 +71,11 @@ void PresetDialog::initPresets()
     ui->presetComboBox->addItem("Default", 0);
     ui->presetComboBox->setCurrentIndex(0);
 
-    if (!dataDir.exists("presets.ini")) accept();
+    if (!dataDir.exists("presets.ini"))
+    {
+        reject();
+        return;
+    }
     QSettings presets(dataDir.filePath("presets.ini"), QSettings::IniFormat, this);
     
     bool ok = true;

app/src/presetdialog.h
@@ -23,9 +23,6 @@ public:
     int getPresetIndex();
     bool shouldAlwaysUse();
 
-public slots:
-    int exec() override;
-
 private:
     void initPresets();
 

commit 17642da33d38c43823d0ac7b503d57e33581f0b7

Fix the always use this prefix checkbox
Looks like it was broken in aa5c844d99ca7ed9735f609e4974ed53bbe2b409

app/src/mainwindow2.cpp
@@ -1071,6 +1071,10 @@ void  MainWindow2::showPresetDialog()
             if (result == QDialog::Accepted)
             {
                 int presetIndex = presetDialog->getPresetIndex();
+                if (presetDialog->shouldAlwaysUse()) {
+                    mEditor->preference()->set(SETTING::ASK_FOR_PRESET, false);
+                    mEditor->preference()->set(SETTING::DEFAULT_PRESET, presetIndex);
+                }
                 newObjectFromPresets(presetIndex);
                 qDebug() << "Accepted!";
             }